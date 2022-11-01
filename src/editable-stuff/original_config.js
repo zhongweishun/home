d347c10ed97e0003fa49f4548224d2d0f8b5f9c7
Weishun Zhong <wszhong@mit.edu>
2022-11-01 15:20:35 -0400

    first commit

--- src/editable-stuff/original_config.js
+++ src/editable-stuff/original_config.js
@@ -6,11 +6,10 @@ const navBar = {
 // Main Body SECTION
 const mainBody = {
   gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
-  firstName: "Weishun",
+  firstName: "Hashir",
   middleName: "",
-  lastName: "Zhong",
-  message: " A statistical physicist wandering in the wild.. ",
-  
+  lastName: "Shoaib",
+  message: " Passionate about changing the world with technology. ",
   icons: [
     {
       image: "fa-github",
@@ -52,10 +51,10 @@ const mainBody = {
 const about = {
   show: true,
   heading: "About Me",
-  imageLink: require("../editable-stuff/weishunzhong.JPG"),
+  imageLink: require("../editable-stuff/hashirshoaeb.png"),
   imageSize: 375,
   message:
-    "My name is Weishun Zhong. I’m a Ph.D. student in physics at MIT, supervised by Haim Sompolinsky and Mehran Kardar.",
+    "My name is Hashir Shoaib. I’m a graduate of 2020 from National University of Sciences and Technology at Islamabad with a degree in Computer Engineering. I'm most passionate about giving back to the community, and my goal is to pursue this passion within the field of software engineering. In my free time I like working on open source projects.",
   resume: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
 };
 
@@ -80,12 +79,12 @@ const leadership = {
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
   images: [
     { 
-      img: require("../editable-stuff/weishunzhong.JPG"), 
+      img: require("../editable-stuff/hashirshoaeb.png"), 
       label: "First slide label", 
       paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
     },
     { 
-      img: require("../editable-stuff/weishunzhong.JPG"), 
+      img: require("../editable-stuff/hashirshoaeb.png"), 
       label: "Second slide label", 
       paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
     },
