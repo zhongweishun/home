68c7f7f98ea2146c53346e71a8aa7951808f36fd
mao <maochfe@gmail.com>
2022-11-13 15:17:47 -0500

    portfolio

--- src/components/home/Experience.jsx
+++ src/components/home/Experience.jsx
@@ -9,7 +9,7 @@ import {
 const Experience = ({ experiences }) => {
   return (
     <section className="section">
-      <Container>
+      <Container className="shadow-lg p-3 mb-5 bg-white rounded">
         <Jumbotron className="bg-white">
           <h2 className="display-4 mb-5 text-center">
             {experiences.heading}
