7e7428db7157c65086b7949fece4740626918be6
Weishun Zhong <weishunzhong.ai@gmail.com>
2024-12-06 00:39:22 -0500

    added RTM animation

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -149,12 +149,13 @@ const portfolio = {
       category: "Mobile App",
       title: "Random tree ensemble for meaningful memory",
       url: "https://arxiv.org/abs/2412.01806",
-      notes: "Semantic comprehension of meaningful material, such as narratives, has traditionally been studied with a focus on content-specific information, \
-              leaving common quantitative features across different narratives largely unexplored. By treating linguistic clauses as the fundamental 'atoms,' \
+      notes: "Semantic comprehension of narratives has traditionally been studied with a focus on content-specific information, \
+              leaving common quantitative features largely unexplored. By treating linguistic clauses as the fundamental 'atoms,' \
               we introduce a statistical mechanics theory of random tree representations to model narratives. Remarkably, our simple toy model captures the \
               statistical regularities observed in large-scale narrative recall experiments without requiring any parameter tuning. Our model further reveals \
               a universal, scale-invariant limit for long narratives, where semantic abstraction emerges naturally as a consequence of the scale-invariance.",
       thumbnail:require("../editable-stuff/RTM.png"), 
+      videoUrl:require("../editable-stuff/RTM_1minute.mp4")
      },
     {
       category: "Mobile App",
